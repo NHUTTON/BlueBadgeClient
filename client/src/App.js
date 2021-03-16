39e788fe7d779c682c81f3469a8ea7c655790925
NHUTTON <nickolashutton@gmail.com>
2021-03-16 12:17:40 -0400

    client in progress

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,30 @@
+import React, { useState, useEffect } from 'react'
 import './App.css';
 
 import Auth from './auth/Auth'
 import  Index from './list/index'
 
-function App() {
+function App() {  
+  const [sessionToken, setSessionToken] = useState('');
+
+  useEffect(() => {
+    if (localStorage.getItem('token')) {
+      setSessionToken(localStorage.getItem('token'))
+    }
+  }, [])
+
+ 
+    const updateToken = (newToken) => {
+      localStorage.setItem('token', newToken);
+      setSessionToken(newToken)
+      console.log(sessionToken)
+    }
+
+    const clearToken = () => {
+      localStorage.clear();
+      setSessionToken('')
+    }
+ 
 
   return (
     <div>
